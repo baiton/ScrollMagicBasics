7557a77998ce3a736d55ec6a887c71232a6ea42d
Brandon Aiton <brandonaiton@icloud.com>
2017-11-06 11:48:38 -0500

    successfully created scrolling intros for elements on page  Pinning effects for jumbotron header and opacity reveals for articles

--- 01-intro/start/js/main.js
+++ 01-intro/start/js/main.js
@@ -2,19 +2,50 @@ $(document).ready(function(){
 
 	let controller = new ScrollMagic.Controller();
 
-	let ourScene =  new ScrollMagic.Scene({
-		triggerElement: '#project01 img',
-		duration: '90%',
-		triggerHook: 0.9
+	//pin intro
+	let pinIntroScene = new ScrollMagic.Scene({
+		triggerElement: '#intro',
+		triggerHook: 0,
+		duration: '30%'
 	})
-	.setClassToggle('#project01', 'fade-in')
+	.setPin('#intro', {pushFollowers: false})
+	.addTo(controller)
+
+	//pin again
+	let pinIntroScene2 = new ScrollMagic.Scene({
+		triggerElement: '#project01',
+		triggerHook: .4,
+	})
+	.setPin('#intro', {pushFollowers: false})
 	.addIndicators({
-		name: 'fade scene',
+		name: 'intro pin',
 		colorTrigger: 'black',
 		// indent: 200,
 		colorStart: '#0e770c',
 		colorEnd: '#0c4377'
-	}) // this requires a different script linked in the html
-	.addTo(controller);
+	})
+	.addTo(controller)
+
+	//loop through each .project element
+	$('.project').each(function(){
+
+		console.log('this', [this]);
+
+		let ourScene =  new ScrollMagic.Scene({
+			triggerElement: this.children[0],
+			// duration: '90%',
+			triggerHook: 0.9,
+			reverse: false
+		})
+		.setClassToggle(this, 'fade-in')
+		.addIndicators({
+			name: 'fade scene',
+			colorTrigger: 'black',
+			// indent: 200,
+			colorStart: '#0e770c',
+			colorEnd: '#0c4377'
+		}) // this requires a different script linked in the html
+		.addTo(controller);
+	})
 
 });
